0292cfcd245975b4e2b53147a0ae5e7f5cd40086
jeehwan <tjtojan@naver.com>
2023-12-25 00:41:22 +0900

    Carousel 컴포넌트 구현

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Carousel from "../components/hotel/Carousel";
+import useHotel from "../components/hotel/hooks/useHotel";
+import Top from "../components/shared/Top";
 
 function Hotel() {
-  return <div>Hotel</div>;
+  const { id } = useParams() as { id: string };
+
+  const { isLoading, data } = useHotel({ id });
+
+  if (data == null || isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  const { name, comment, images } = data;
+  return (
+    <div>
+      <Top title={name} subTitle={comment} />
+      <Carousel images={images} />
+    </div>
+  );
 }
 
 export default Hotel;
