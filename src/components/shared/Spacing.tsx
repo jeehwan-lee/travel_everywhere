325c6dceb0df1bfca8c66e02c2213e074a06c175
jeehwan <tjtojan@naver.com>
2023-12-24 15:53:33 +0900

    Hotel List 페이지 구현

--- src/components/shared/Spacing.tsx
+++ src/components/shared/Spacing.tsx
@@ -1,17 +1,22 @@
 /** @jsxImportSource @emotion/react */
 
 import styled from "@emotion/styled";
+import { Colors, colors } from "../../styles/colorPalette";
 
 interface SpacingProps {
   size: number;
   direction?: "vertical" | "horizontal";
+  backgroundColor?: Colors;
 }
 
 export const Spacing = styled.div<SpacingProps>`
   ${({ size, direction = "vertical" }) =>
     direction === "vertical"
       ? `
-    height : ${size}px`
+    height : ${size}px;`
       : `
-      width : ${size}px`}
+      width : ${size}px;`}
+
+  ${({ backgroundColor }) =>
+    backgroundColor && `background-color : ${colors[backgroundColor]};`}
 `;
--- src/pages/HotelList.tsx
+++ src/pages/HotelList.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+
+import React, { Fragment } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import useHotels from "../components/hotel_list/hooks/useHotels";
 import Hotel from "../components/hotel_list/Hotel";
+import { Spacing } from "../components/shared/Spacing";
 import Top from "../components/shared/Top";
 
 function HotelList() {
-  const { data: hotels } = useHotels();
+  const { data: hotels, hasNextPage, loadMore } = useHotels();
 
   console.log(hotels);
   return (
     <div>
       <Top title="인기호텔" subTitle="호텔부터 펜션까지 최저가" />
 
-      {hotels?.map((hotel) => (
-        <Hotel key={hotel.id} hotel={hotel} />
-      ))}
+      <InfiniteScroll
+        dataLength={hotels?.length ?? 0}
+        hasMore={hasNextPage}
+        loader={<></>}
+        next={loadMore}
+        scrollThreshold="100px"
+      >
+        <ul>
+          {hotels?.map((hotel, idx) => (
+            <Fragment key={hotel.id}>
+              <Hotel hotel={hotel} />
+              {hotels.length - 1 === idx ? null : (
+                <Spacing size={10} backgroundColor="gray100" />
+              )}
+            </Fragment>
+          ))}
+        </ul>
+      </InfiniteScroll>
     </div>
   );
 }
