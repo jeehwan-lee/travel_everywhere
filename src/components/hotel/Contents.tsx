386bd926e7758fa4d33023765c89d7673da4549b
jeehwan <tjtojan@naver.com>
2023-12-25 00:48:22 +0900

    Hotel 상세 내용 추가

---
+++ src/components/hotel/Contents.tsx
@@ -0,0 +1,44 @@
+import styled from "@emotion/styled";
+import ReactMarkdown from "react-markdown";
+import { typographyMap } from "../../styles/typography";
+
+function Contents({ contents }: { contents: string }) {
+  return (
+    <ContainerStyles>
+      <ReactMarkdown>{contents}</ReactMarkdown>
+    </ContainerStyles>
+  );
+}
+
+const ContainerStyles = styled.div`
+  padding: 24px;
+  ${typographyMap.t6};
+
+  h2 {
+    ${typographyMap.t4}
+    font-weight : bold;
+    margin: 18px 0;
+  }
+
+  ul {
+    padding-inline-start: 20px;
+    margin: 18px 0;
+  }
+
+  li {
+    list-style-type: disc;
+  }
+
+  p {
+    margin: 18px 0;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 8px;
+  }
+`;
+
+export default Contents;
--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "../components/hotel/Carousel";
+import Contents from "../components/hotel/Contents";
 import useHotel from "../components/hotel/hooks/useHotel";
 import Top from "../components/shared/Top";
 
@@ -13,11 +14,12 @@ function Hotel() {
     return <div>Loading...</div>;
   }
 
-  const { name, comment, images } = data;
+  const { name, comment, images, contents } = data;
   return (
     <div>
       <Top title={name} subTitle={comment} />
       <Carousel images={images} />
+      <Contents contents={contents} />
     </div>
   );
 }
