77fe78eddaf93ee7067549b01d724bde8e1c1340
jeehwan <tjtojan@naver.com>
2023-12-25 01:59:00 +0900

    실시간으로 room data 반영

--- src/components/hotel/hooks/useRooms.ts
+++ src/components/hotel/hooks/useRooms.ts
@@ -1,7 +1,28 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import { getRooms } from "../../../remote/room";
+import { useEffect } from "react";
+import { collection, doc, onSnapshot } from "firebase/firestore";
+import { store } from "../../../remote/firebase";
+import { COLLECTIONS } from "../../../constants";
+import { Room } from "../../../models/room";
 
 function useRooms({ hotelId }: { hotelId: string }) {
+  const client = useQueryClient();
+
+  useEffect(() => {
+    onSnapshot(
+      collection(doc(store, COLLECTIONS.HOTEL, hotelId), COLLECTIONS.ROOM),
+      (snapshot) => {
+        const newRooms = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...(doc.data() as Room),
+        }));
+
+        client.setQueriesData(["rooms", hotelId], newRooms);
+      }
+    );
+  }, [hotelId, client]);
+
   return useQuery(["rooms", hotelId], () => getRooms(hotelId));
 }
 
