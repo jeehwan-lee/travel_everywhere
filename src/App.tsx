a362d5340355ad67ee4894c1ccdcec9fbddf2331
jeehwan <tjtojan@naver.com>
2023-12-25 12:38:36 +0900

    공유하기 hook 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useLoadKaKao from "./hooks/useLoadKaKao";
 import Hotel from "./pages/Hotel";
 import HotelList from "./pages/HotelList";
 import Test from "./pages/Test";
 
 function App() {
+  useLoadKaKao();
   return (
     <BrowserRouter>
       <Routes>
