0981fc9dbea8849cdc7cb4b901089eb9c765c82d
jeehwan <tjtojan@naver.com>
2023-12-25 14:14:37 +0900

    Google Login 기능 추가

--- src/hooks/useGoogleSignin.ts
+++ src/hooks/useGoogleSignin.ts
@@ -1,15 +1,39 @@
+import { FirebaseError } from "firebase/app";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
 import { useCallback } from "react";
-import { auth } from "../remote/firebase";
+import { useNavigate } from "react-router-dom";
+import { COLLECTIONS } from "../constants";
+import { auth, store } from "../remote/firebase";
 
 function useGoogleSignin() {
+  const navigate = useNavigate();
+
   const signin = useCallback(async () => {
     const provider = new GoogleAuthProvider();
 
     try {
       const { user } = await signInWithPopup(auth, provider);
-      console.log("user", user);
-    } catch (error) {}
+
+      const newUser = {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        photoUrl: user.photoURL,
+      };
+
+      await setDoc(doc(collection(store, COLLECTIONS.USER), user.uid), newUser);
+
+      navigate("/");
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        if (error.code === "auth/popup-closed-by-user") {
+          return;
+        }
+      }
+
+      throw new Error("Fail to SignIn");
+    }
   }, []);
 
   const signout = useCallback(() => {
