d4bfad82e8dc321878652845372eb22dd8497105
jeehwan <tjtojan@naver.com>
2023-12-26 01:23:59 +0900

    Navbar에 세팅버튼 추가

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,6 +7,7 @@ import { css } from "@emotion/react";
 import { colors } from "../../styles/colorPalette";
 import { useCallback } from "react";
 import useUser from "../../hooks/auth/userUser";
+import { Spacing } from "./Spacing";
 
 function Navbar() {
   const location = useLocation();
@@ -19,18 +20,29 @@ function Navbar() {
     if (user != null) {
       return (
         <>
-          <Link to="/my">
-            <img
-              src={
-                user.photoURL ??
-                "https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_user2-64.png"
-              }
-              alt=""
-              width={40}
-              height={40}
-              style={{ borderRadius: "100%" }}
-            />
-          </Link>
+          <Flex justify="center" align="center">
+            <Link to="/my">
+              <img
+                src={
+                  user.photoURL ??
+                  "https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_user2-64.png"
+                }
+                alt=""
+                width={40}
+                height={40}
+                style={{ borderRadius: "100%" }}
+              />
+            </Link>
+            <Spacing size={8} direction="horizontal" />
+            <Link to="/settings">
+              <img
+                src="https://cdn4.iconfinder.com/data/icons/evil-icons-user-interface/64/setting-64.png"
+                alt=""
+                width={50}
+                height={50}
+              />
+            </Link>
+          </Flex>
         </>
       );
     }
