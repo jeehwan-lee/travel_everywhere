b9d78e653ddae1eaae62c37e27a0a94f6f527bcb
jeehwan <tjtojan@naver.com>
2023-12-29 01:09:49 +0900

    calender style 적용

--- src/components/shared/RangePicker.tsx
+++ src/components/shared/RangePicker.tsx
@@ -1,6 +1,8 @@
 import { DayPicker, DateRange } from "react-day-picker";
 import { ko } from "date-fns/locale";
 import { differenceInDays, format, isSameDay, parseISO } from "date-fns";
+import styled from "@emotion/styled";
+import { colors } from "../../styles/colorPalette";
 
 interface RangePickerProps {
   startDate?: string;
@@ -44,15 +46,85 @@ function RangePicker({ startDate, endDate, onChange }: RangePickerProps) {
   };
 
   return (
-    <DayPicker
-      locale={ko}
-      mode="range"
-      numberOfMonths={5}
-      defaultMonth={today}
-      onSelect={handleDayClick}
-      selected={selected}
-    />
+    <Container>
+      <DayPicker
+        locale={ko}
+        mode="range"
+        numberOfMonths={5}
+        defaultMonth={today}
+        onSelect={handleDayClick}
+        selected={selected}
+      />
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding-bottom: 80px;
+
+  .rdp-month {
+    position: relative;
+    width: 100%;
+    text-align: center;
+    padding: 60px 0px 30px;
+  }
+
+  .rdp-caption {
+    position: absolute;
+    top: 25px;
+    left: 20px;
+    color: ${colors.black};
+    font-weight: bold;
+  }
+
+  .rdp-nav {
+    display: none;
+  }
+
+  .rdp-table {
+    width: 100%;
+  }
+
+  .rdp-head .rdp-head_row {
+    font-size: 12px;
+    height: 45px;
+    color: ${colors.gray400};
+    font-weight: bold;
+  }
+
+  .rdp-tbody .rdp-row {
+    height: 45px;
+  }
+
+  .rdp-cell .rdp-button {
+    position: relative;
+    width: 100%;
+    line-height: 45px;
+  }
+
+  .rdp-day_selected {
+    background-color: ${colors.blue100};
+  }
+
+  .rdp-cell .rdp-day_range_start,
+  .rdp-cell .rdp-day_range_end {
+    color: ${colors.white};
+  }
+
+  .rdp-cell .rdp-day_range_start::after,
+  .rdp-cell .rdp-day_range_end::after {
+    z-index: -1;
+    display: block;
+    width: calc(100% - 1px);
+    height: 45px;
+    position: absolute;
+    top: 50%;
+    bottom: 0px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: ${colors.blue};
+    content: "";
+  }
+`;
 
 export default RangePicker;
