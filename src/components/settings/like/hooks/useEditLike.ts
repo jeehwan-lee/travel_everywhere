aab10419901bb327b1980427fa5f58fc99369245
jeehwan <tjtojan@naver.com>
2023-12-28 10:24:38 +0900

    찜한 데이터 Update API 구현

--- src/components/settings/like/hooks/useEditLike.ts
+++ src/components/settings/like/hooks/useEditLike.ts
@@ -1,10 +1,13 @@
 import { useCallback, useState } from "react";
+import { useAlertContext } from "../../../../contexts/AlertContext";
 import useLike from "../../../../hooks/useLike";
 import { Like } from "../../../../models/like";
+import { updateOrder } from "../../../../remote/like";
 
 function useEditLike() {
   const { data = [] } = useLike();
   const [updatedLikes, setUpdatedLikes] = useState<Like[]>([]);
+  const { open } = useAlertContext();
 
   const isEdit = updatedLikes.length > 0;
 
@@ -23,9 +26,21 @@ function useEditLike() {
     [data]
   );
 
-  const save = () => {};
+  const save = async () => {
+    try {
+      await updateOrder(updatedLikes);
+      setUpdatedLikes([]);
+    } catch {
+      open({
+        title: "알 수 없는 에러가 발생했습니다.",
+        onButtonClick: () => {
+          setUpdatedLikes([]);
+        },
+      });
+    }
+  };
 
-  return { data: isEdit ? updatedLikes : data, isEdit, reorder };
+  return { data: isEdit ? updatedLikes : data, isEdit, reorder, save };
 }
 
 export default useEditLike;
--- src/pages/settings/Like.tsx
+++ src/pages/settings/Like.tsx
@@ -9,10 +9,9 @@ import {
 import useEditLike from "../../components/settings/like/hooks/useEditLike";
 import FixedBottomButton from "../../components/shared/FixedBottomButton";
 import ListRow from "../../components/shared/ListRow";
-import useLike from "../../hooks/useLike";
 
 function LikePage() {
-  const { data, isEdit, reorder } = useEditLike();
+  const { data, isEdit, reorder, save } = useEditLike();
 
   const handleDragEndDrop = (result: DropResult) => {
     if (result.destination == null) {
@@ -62,10 +61,7 @@ function LikePage() {
         </StrickModeDroppable>
       </DragDropContext>
       {isEdit ? (
-        <FixedBottomButton
-          label="저장하기"
-          onClick={() => console.log("hello")}
-        />
+        <FixedBottomButton label="저장하기" onClick={() => save()} />
       ) : null}
     </>
   );
