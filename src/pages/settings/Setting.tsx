780b2bd5b400ee60602262ff9c9208bca0de7399
jeehwan <tjtojan@naver.com>
2023-12-28 09:56:15 +0900

    Like 페이지 생성

--- src/pages/settings/Setting.tsx
+++ src/pages/settings/Setting.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import ListRow from "../../components/shared/ListRow";
 
 function SettingPage() {
-  return <div>index</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/settings/like">
+            <ListRow
+              as="div"
+              contents={
+                <ListRow.Texts title="찜하기" subTitle="찜한 호텔 순서변경" />
+              }
+              withArrow={true}
+            />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SettingPage;
