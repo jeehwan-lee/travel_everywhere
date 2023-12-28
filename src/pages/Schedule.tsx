e4769fb27c8dc82522da8fad68f2897406699b37
jeehwan <tjtojan@naver.com>
2023-12-29 01:14:37 +0900

    Calendar를 통한 예약정보 선택

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,11 +1,15 @@
 import qs from "qs";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import FixedBottomButton from "../components/shared/FixedBottomButton";
 import RangePicker from "../components/shared/RangePicker";
 
 function Schedule() {
   const { roomId, hotelId } = qs.parse(window.location.search, {
     ignoreQueryPrefix: true,
   }) as { roomId: string; hotelId: string };
+
+  const navigate = useNavigate();
 
   const [selectedDate, setSelectedDate] = useState<{
     startDate?: string;
@@ -16,6 +20,26 @@ function Schedule() {
     endDate: undefined,
     nights: 0,
   });
+
+  const isSubmit =
+    selectedDate.startDate != null && selectedDate.endDate != null;
+
+  const buttonLabel = isSubmit
+    ? `${selectedDate.startDate} - ${selectedDate.endDate} (${selectedDate.nights}박)`
+    : "예약날짜를 선택해주세요";
+
+  const moveToReservationPage = () => {
+    const params = qs.stringify(
+      {
+        hotelId,
+        roomId,
+        ...selectedDate,
+      },
+      { addQueryPrefix: true }
+    );
+
+    navigate(`/reservation${params}`);
+  };
 
   useEffect(() => {
     if (roomId === "" || hotelId === "") {
@@ -35,6 +59,11 @@ function Schedule() {
             nights: dateRange.nights,
           });
         }}
+      />
+      <FixedBottomButton
+        label={buttonLabel}
+        disabled={isSubmit === false}
+        onClick={() => moveToReservationPage()}
       />
     </div>
   );
