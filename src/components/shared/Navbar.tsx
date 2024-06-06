7569f8d834fec511ff2202c48d63a10b5b4451e9
jeehwan <tjtojan@naver.com>
2024-06-06 23:12:37 +0900

    프로필 드롭다운 메뉴 생성

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,7 +5,7 @@ import Flex from "./Flex";
 import Button from "./Button";
 import { css } from "@emotion/react";
 import { colors } from "../../styles/colorPalette";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import useUser from "../../hooks/auth/userUser";
 import { Spacing } from "./Spacing";
 import useGoogleSignin from "../../hooks/useGoogleSignin";
@@ -15,30 +15,27 @@ import Text from "./Text";
 import { Input } from "./Input";
 import SearchInput from "./SearchInput";
 import { FaHome } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
+import { FaCaretUp } from "react-icons/fa";
 
 function Navbar() {
   const location = useLocation();
   const navigate = useNavigate();
   const user = useUser();
 
+  const [showDropDownMenu, setShowDropDownMenu] = useState<Boolean>(false);
+
+  const profileClick = () => {
+    setShowDropDownMenu((prev) => !prev);
+  };
+
   const renderButton = useCallback(() => {
     if (user != null) {
       return (
         <>
           <Flex justify="center" align="center">
-            <Button onClick={() => navigate("/register/Hotel")}>
-              등록하기
-            </Button>
-            <Button
-              onClick={() => {
-                signOut(auth);
-              }}
-            >
-              로그아웃
-            </Button>
             <Spacing size={8} direction="horizontal" />
-            <Text>{user.displayName}</Text>
-            <Link to="/settings">
+            <Flex align="center" onClick={profileClick} css={profileStyles}>
               <img
                 src={
                   user.photoURL ??
@@ -49,7 +46,46 @@ function Navbar() {
                 height={40}
                 style={{ borderRadius: "100%" }}
               />
-            </Link>
+              <Spacing size={10} direction="horizontal" />
+              <Text typography="t6">{user.displayName}님</Text>
+              <Spacing size={4} direction="horizontal" />
+              {showDropDownMenu ? (
+                <>
+                  <FaCaretUp size={18} />
+                  <Flex css={dropDownStyles} direction="column">
+                    <Text
+                      typography="t5"
+                      css={dropDownTextStyles}
+                      onClick={() => navigate("/register/Hotel")}
+                    >
+                      호텔 등록하기
+                    </Text>
+                    <Spacing size={6} />
+                    <Text
+                      typography="t5"
+                      css={dropDownTextStyles}
+                      onClick={() => navigate("/profile")}
+                    >
+                      마이페이지
+                    </Text>
+                    <Spacing size={6} />
+                    <Text
+                      typography="t5"
+                      css={dropDownTextStyles}
+                      onClick={() => {
+                        signOut(auth);
+                      }}
+                    >
+                      로그아웃
+                    </Text>
+                  </Flex>
+                </>
+              ) : (
+                <>
+                  <FaCaretDown size={18} />
+                </>
+              )}
+            </Flex>
             <Spacing size={8} direction="horizontal" />
           </Flex>
         </>
@@ -61,7 +97,8 @@ function Navbar() {
         <Button css={buttonStyles}>로그인 및 회원가입</Button>
       </Link>
     );
-  }, [user]);
+  }, [user, showDropDownMenu]);
+
   return (
     <div css={headerStyles}>
       <Flex direction="column" css={navbarStyles}>
@@ -99,6 +136,32 @@ const navbarStyles = css`
 const buttonStyles = css`
   font-size: 14px;
   padding: 10px 20px;
+`;
+
+const profileStyles = css`
+  position: relative;
+  cursor: pointer;
+`;
+
+const dropDownStyles = css`
+  position: absolute;
+  top: 50px;
+  border: 1px solid ${colors.gray200};
+  border-radius: 4px;
+  width: 180px;
+  background-color: ${colors.white};
+  padding: 10px 20px;
+  z-index: 999;
+  cursor: default;
+`;
+
+const dropDownTextStyles = css`
+  cursor: pointer;
+
+  &:hover {
+    font-size: 18px;
+    font-weight: bold;
+  }
 `;
 
 export default Navbar;
